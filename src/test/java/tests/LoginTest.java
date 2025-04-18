d6d9d1aa728f5ba58f116949a1908a1c7df950b3
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2025-04-18 19:51:54 +0300

    add env properties in LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -37,6 +37,6 @@ public class LoginTest extends Preconditions implements ITestConstants {
     @Test(description = "Login user with correct username and password")
     public void successfulLoginTest() {
         loginSteps.loginAndWaitForPageOpened(userSuccessLogin);
-        headerSteps.checkWelcomeMessageInHeader(USER);
+        headerSteps.checkWelcomeMessageInHeader(System.getenv("username"));
     }
 }
