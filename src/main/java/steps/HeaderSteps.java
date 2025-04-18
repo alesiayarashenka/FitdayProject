10d898dce2bc197362ed9eb2892de1faa6c700b8
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2025-04-18 22:22:39 +0300

    loginTest5

--- src/main/java/steps/HeaderSteps.java
+++ src/main/java/steps/HeaderSteps.java
@@ -18,8 +18,8 @@ public class HeaderSteps extends BaseSteps {
     }
 
     @Step("Check welcome message in header on forum page after login")
-    public HeaderSteps checkWelcomeMessageInHeader(String user) {
-        headerPage.isForumsOpened(user);
+    public HeaderSteps checkWelcomeMessageInHeader() {
+        headerPage.isForumsOpened();
         return this;
     }
 
