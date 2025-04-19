f348d05dfa250b32a4e1722e4d4855d3e740019d
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2025-04-19 08:52:08 +0300

    update method

--- src/main/java/pages/HeaderPage.java
+++ src/main/java/pages/HeaderPage.java
@@ -43,11 +43,27 @@ public class HeaderPage extends BasePage {
      * @return
      */
     public boolean isForumsOpened(String user) {
-        waiter.waitForPageOpened((By.xpath(String.format(WELCOME_MEMBER_XPATH, user))), driver);
-        WebElement welcomeMessage = driver.findElement(By.xpath(String.format(WELCOME_MEMBER_XPATH, user)));
-        log.info("Welcome message with name {} is displayed", System.getenv("username"));
-//        log.info("Welcome message with name {} is displayed", user);  //---for local
-        return welcomeMessage.isDisplayed();
+//        waiter.waitForPageOpened((By.xpath(String.format(WELCOME_MEMBER_XPATH, user))), driver);
+//        WebElement welcomeMessage = driver.findElement(By.xpath(String.format(WELCOME_MEMBER_XPATH, user)));
+//        log.info("Welcome message with name {} is displayed", System.getenv("username"));
+        log.info("Welcome message with name {} is displayed", user);  //---for local
+//        return welcomeMessage.isDisplayed();
+
+        int i = 0;
+        while (i < 50) {
+            try {
+                if (!driver.findElement(By.xpath(String.format(WELCOME_MEMBER_XPATH, user))).isDisplayed()) {
+                    Thread.sleep(500);
+                    ++i;
+                }
+                if (driver.findElement(By.xpath(String.format(WELCOME_MEMBER_XPATH, user))).isDisplayed()) {
+                    break;
+                }
+            } catch (Exception a) {
+                break;
+            }
+        }
+            return driver.findElement(By.xpath(String.format(WELCOME_MEMBER_XPATH, user))).isDisplayed();
     }
 
     /**
